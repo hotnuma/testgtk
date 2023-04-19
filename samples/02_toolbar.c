c795018097ac1c04a01dce0c8ae5236458d207e3
hotnuma <hotnuma@gmail.com>
2023-04-19 09:25:23 +0200

    add a toolbar menu button

--- samples/02_toolbar.c
+++ samples/02_toolbar.c
@@ -26,22 +26,41 @@ int main(int argc, char **argv)
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     gtk_container_add(GTK_CONTAINER(window), vbox);
 
+
     GtkWidget *toolbar = gtk_toolbar_new();
     gtk_toolbar_set_style(GTK_TOOLBAR(toolbar), GTK_TOOLBAR_ICONS);
+    GtkToolItem *toolitem = NULL;
+    GtkWidget *item = NULL;
+    GtkWidget *menu = NULL;
 
-    GtkToolItem *item = NULL;
+    // GtkMenuToolButton
+    G_GNUC_BEGIN_IGNORE_DEPRECATIONS;
+    toolitem = gtk_menu_tool_button_new_from_stock("gtk-open");
+    G_GNUC_END_IGNORE_DEPRECATIONS;
+    menu = create_menu();
+    gtk_menu_tool_button_set_menu(GTK_MENU_TOOL_BUTTON(toolitem), menu);
+    gtk_toolbar_insert(GTK_TOOLBAR(toolbar), toolitem, -1);
 
-    item = gtk_menu_tool_button_new_from_stock("gtk-open");
-    GtkWidget *menu = create_menu();
-    gtk_menu_tool_button_set_menu(GTK_MENU_TOOL_BUTTON(item), menu);
-    gtk_toolbar_insert(GTK_TOOLBAR(toolbar), item, -1);
+    // GtkMenuButton
+    toolitem  = gtk_tool_item_new();
+    item = gtk_menu_button_new();
+    GtkWidget *image = gtk_image_new_from_icon_name("media-playback-start",
+                                                    GTK_ICON_SIZE_BUTTON);
+    gtk_button_set_image(GTK_BUTTON(item), image);
+    gtk_button_set_relief(GTK_BUTTON(item), GTK_RELIEF_NONE);
+    gtk_container_add(GTK_CONTAINER(toolitem), item);
+    menu = create_menu();
+    gtk_menu_button_set_popup(GTK_MENU_BUTTON(item), menu);
+    gtk_toolbar_insert(GTK_TOOLBAR(toolbar), toolitem, -1);
 
-    item  = gtk_tool_item_new();
-    GtkWidget *entry = gtk_entry_new();
-    gtk_container_add(GTK_CONTAINER(item), entry);
-    gtk_toolbar_insert(GTK_TOOLBAR(toolbar), item, -1);
+    // GtkEntry
+    toolitem  = gtk_tool_item_new();
+    item = gtk_entry_new();
+    gtk_container_add(GTK_CONTAINER(toolitem), item);
+    gtk_toolbar_insert(GTK_TOOLBAR(toolbar), toolitem, -1);
 
     gtk_box_pack_start(GTK_BOX(vbox), toolbar, FALSE, FALSE, 5);
+
 
     g_signal_connect(G_OBJECT(window), "destroy",
                      G_CALLBACK(gtk_main_quit), NULL);
