a88f545dc4de9c34cb664c68c25d8d098d564e91
hotnuma <hotnuma@gmail.com>
2023-04-28 19:08:25 +0200

    zetcode samples : gtk3 port 8

--- zetcode/081_separator.c
+++ zetcode/081_separator.c
@@ -2,43 +2,29 @@
 
 int main(int argc, char **argv)
 {
-
-    GtkWidget *window;
-    GtkWidget *label1;
-    GtkWidget *label2;
-    GtkWidget *hseparator;
-    GtkWidget *vbox;
-
     gtk_init(&argc, &argv);
 
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "GtkHSeparator");
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
-
     gtk_container_set_border_width(GTK_CONTAINER(window), 10);
 
-    label1 = gtk_label_new("Zinc is a moderately reactive, blue gray metal \
-that tarnishes in moist air and burns in air with a bright bluish-green flame,\
-giving off fumes of zinc oxide. It reacts with acids, alkalis and other non-metals.\
-If not completely pure, zinc reacts with dilute acids to release hydrogen.");
-
-    gtk_label_set_line_wrap(GTK_LABEL(label1), TRUE);
-
-    label2 = gtk_label_new("Copper is an essential trace nutrient to all high \
-plants and animals. In animals, including humans, it is found primarily in \
-the bloodstream, as a co-factor in various enzymes, and in copper-based pigments. \
-However, in sufficient amounts, copper can be poisonous and even fatal to organisms.");
-
-    gtk_label_set_line_wrap(GTK_LABEL(label2), TRUE);
-
-    vbox = gtk_vbox_new(FALSE, 10);
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     gtk_container_add(GTK_CONTAINER(window), vbox);
 
-    hseparator = gtk_hseparator_new();
-
+    GtkWidget *label1;
+    label1 = gtk_label_new("Zinc is a moderately reactive, blue gray metal.");
+    gtk_label_set_line_wrap(GTK_LABEL(label1), TRUE);
     gtk_box_pack_start(GTK_BOX(vbox), label1, FALSE, TRUE, 0);
+
+    GtkWidget *hseparator = gtk_separator_new(GTK_ORIENTATION_HORIZONTAL);
     gtk_box_pack_start(GTK_BOX(vbox), hseparator, FALSE, TRUE, 10);
+
+    GtkWidget *label2;
+    label2 = gtk_label_new("Copper is an essential trace nutrient to all high"
+                           " plants and animals.");
+    gtk_label_set_line_wrap(GTK_LABEL(label2), TRUE);
     gtk_box_pack_start(GTK_BOX(vbox), label2, FALSE, TRUE, 0);
 
     g_signal_connect_swapped(G_OBJECT(window), "destroy",
